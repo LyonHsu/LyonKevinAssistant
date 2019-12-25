82b5e270e6842c733d28330217ddba1cf0661b8b
Lyon <lejiteyu@gmail.com>
2019-12-25 15:22:14 +0800

    20191225 新增NetWork Menu 可以選擇wifi 連線

--- app/src/main/java/lyon/kevin/googlethings/assistant/MainActivity.java
+++ app/src/main/java/lyon/kevin/googlethings/assistant/MainActivity.java
@@ -1,17 +1,37 @@
 package lyon.kevin.googlethings.assistant;
 
 import android.content.Context;
+import android.media.AudioManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+import lyon.kevin.googlethings.assistant.NetWork.NetWork;
+import lyon.kevin.googlethings.assistant.TextToSpeech.LyonTextToSpeech;
 import lyon.kevin.googlethings.assistant.Tool.Alert;
+import lyon.kevin.googlethings.assistant.Tool.Log;
+import lyon.kevin.googlethings.assistant.Tool.MainConstant;
 import lyon.kevin.googlethings.assistant.Tool.Permission;
+import lyon.kevin.googlethings.assistant.Tool.ToastUtile;
 
 public class MainActivity extends AssistantActivity {
     String TAG = MainActivity.class.getSimpleName();
     Context context;
     //Is Use Google AIY Device
     public static boolean isGoogleAIY = true;
-
+    NetWork netWork;
+    final int OPENBLUETOOTH = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +42,112 @@ public class MainActivity extends AssistantActivity {
             Alert.showAlert(this, getString(R.string.AudioRecordtitle), getString(R.string.AudioRecordPermission), "ok");
         }else {
             setContentView(R.layout.activity_main);
+            setTurnScreenOn(true);
+            setTitle(getString(R.string.app_name)+" Hot key:"+ MainConstant.ACTIVATION_KEYPHRASE);
+            textToSpeech= new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+                @Override
+                public void onInit(int status) {
+                    Log.d(TAG, "getTextToSpeech TTS init status:" + status);
+                    if (status != TextToSpeech.ERROR) {
+//                        int result = textToSpeech.setLanguage(Locale.getDefault());//Locale.);
+                        textToSpeech.setPitch(1.0f); // 音調
+                        textToSpeech.setSpeechRate(1.0f); // 速度
+                        int result = textToSpeech.setLanguage(Locale.getDefault());
+                        HashMap myHash = new HashMap<String, String>();
+                        myHash.put(TextToSpeech.Engine.KEY_PARAM_STREAM,
+                                String.valueOf(AudioManager.MODE_NORMAL));
+//                        textToSpeech.speak(openComplete, TextToSpeech.QUEUE_FLUSH, myHash);
+                        Log.d(TAG, "getTextToSpeech speak result init:" + result);
+
+
+                    }else{
+                        Log.e(TAG, "getTextToSpeech TTS init Error:" + status);
+                        ToastUtile.showText(context,"getTextToSpeech TTS init Error:" + status);
+                    }
+                }
+            });
+            netWork = (NetWork) findViewById(R.id.network);
+            netWork.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    netWork.superOnClick();
+                    LyonTextToSpeech.speak(context,textToSpeech,netWork.getLocalIpAddress(context));
+
+                }
+            });
+            netWork.setOnWifiStatusListener(new NetWork.OnWifiStatusListener() {
+                @Override
+                public void wifiStatue(NetworkInfo.DetailedState status) {
+                    Log.i(TAG, " onReceive: intent action wifiStatue:" + status);
+                    if(status.equals(NetworkInfo.DetailedState.CONNECTED)) {
+                        String ipp =  "no connect wifi!";
+                        String ip =ipp;
+                        WifiManager wifiMan = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+                        WifiInfo wifiInf = wifiMan.getConnectionInfo();
+                        int ipAddress = wifiInf.getIpAddress();
+                        ip= String.format("%d.%d.%d.%d", (ipAddress & 0xff),(ipAddress >> 8 & 0xff),(ipAddress >> 16 & 0xff),(ipAddress >> 24 & 0xff));
+                        String IP=""+ip;
+                        String ssID = wifiInf.getSSID();
+                        JSONObject jsonObject = new JSONObject();
+                        try {
+                            jsonObject.put("IP", IP);
+                            jsonObject.put("SSID", ssID);
+                            blueToothWrite(jsonObject);
+                        }catch (JSONException e){
+                            Log.e(TAG,"");
+                        }
+                    }else if(status.equals(NetworkInfo.DetailedState.SCANNING) ||
+                            status.equals(NetworkInfo.DetailedState.DISCONNECTING) ||
+                            status.equals(NetworkInfo.DetailedState.FAILED) ||
+                            status.equals(NetworkInfo.DetailedState.BLOCKED) ||
+                            status.equals(NetworkInfo.DetailedState.DISCONNECTED)
+                    ){
+                        JSONObject jsonObject = new JSONObject();
+                        try {
+                            jsonObject.put("IP", "DISCONNECTED");
+                            blueToothWrite(jsonObject);
+                        }catch (JSONException e){
+                            Log.e(TAG,"");
+                        }
+                    }else if(status.equals(NetworkInfo.DetailedState.CONNECTING) ||
+                            status.equals(NetworkInfo.DetailedState.AUTHENTICATING) ||
+                            status.equals(NetworkInfo.DetailedState.OBTAINING_IPADDR)
+                    ){
+                        JSONObject jsonObject = new JSONObject();
+                        try {
+                            jsonObject.put("IP", "CONNECTING");
+                            blueToothWrite(jsonObject);
+                        }catch (JSONException e){
+                            Log.e(TAG,"");
+                        }
+                    }else if(status.equals(NetworkInfo.DetailedState.SUSPENDED)){
+                        JSONObject jsonObject = new JSONObject();
+                        try {
+                            jsonObject.put("IP", "SUSPENDED");
+                            blueToothWrite(jsonObject);
+                        }catch (JSONException e){
+                            Log.e(TAG,"");
+                        }
+                    }
+                }
+            });
+
             super.onCreate(savedInstanceState);
         }
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message message) {
+            switch (message.what) {
+                case OPENBLUETOOTH:
+                    Log.d(TAG, "handler openBluetoothTime:" + message.obj + "s");
+                    break;
+            }
+        }
+    };
+
+    public void blueToothWrite(JSONObject jsonObject){
+
     }
 }
---
+++ app/src/main/java/lyon/kevin/googlethings/assistant/NetWork/NetWork.java
@@ -0,0 +1,190 @@
+package lyon.kevin.googlethings.assistant.NetWork;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.NetworkInfo;
+import android.net.wifi.SupplicantState;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import lyon.kevin.googlethings.assistant.AppController;
+import lyon.kevin.googlethings.assistant.NetWork.WifiSetting.WifiMenu;
+import lyon.kevin.googlethings.assistant.R;
+import lyon.kevin.googlethings.assistant.Tool.Log;
+
+import static android.net.wifi.WifiManager.EXTRA_SUPPLICANT_ERROR;
+
+/*
+adb shell am start -n  com.example.androidthings.assistant/.AssistantActivity
+adb shell am start -n  com.android.iotlauncher/.DefaultIoTLauncher
+ */
+
+public class NetWork extends RelativeLayout {
+    String TAG = NetWork.class.getSimpleName();
+    Context context ;
+    View view;
+    ImageView netWorkIcon;
+    TextView SSIDTxt,IPTxt,networkTxt;
+    OnWifiStatusListener onWifiStatusListener=null;
+    public static interface OnWifiStatusListener{
+        void wifiStatue(NetworkInfo.DetailedState status);
+    }
+
+    public void setOnWifiStatusListener(OnWifiStatusListener listener) {
+        this.onWifiStatusListener = listener;
+    }
+
+
+    public void onResume(){
+        getLocalIpAddress(context);
+    }
+
+    public NetWork(Context context) {
+        super(context);
+        this.context=context;
+        init();
+    }
+
+    public NetWork(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context=context;
+        init();
+    }
+
+    public NetWork(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.context=context;
+        init();
+    }
+
+
+
+    private void init(){
+        view=inflate(context, R.layout.network_layout,null);
+        setClickable(true);
+        setFocusable(true);
+        setBackground(context.getResources().getDrawable(R.drawable.btn_blue));
+        netWorkIcon = (ImageView)view.findViewById(R.id.netWorkIcon);
+        SSIDTxt = (TextView)view.findViewById(R.id.SSIDTxt);
+        IPTxt = (TextView)view.findViewById(R.id.IPTxt);
+        networkTxt = (TextView)view.findViewById(R.id.networkTxt);
+        networkTxt.setText("Network:"+"    ver:"+getAppVersionName(context)+" "+getAppVersion(context));
+        addView(view);
+        getLocalIpAddress(context);
+//        setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                superOnClick();
+//            }
+//        });
+
+        // 註冊監聽並通過返回重新整理WiFiList
+        IntentFilter wifiFilter = new IntentFilter();
+        wifiFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        wifiFilter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        wifiFilter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION);
+        wifiFilter.addAction(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION);
+        context.registerReceiver(wifiReceiver, wifiFilter);
+    }
+    private BroadcastReceiver wifiReceiver = new BroadcastReceiver(){
+        public void onReceive(Context context, Intent intent) {
+            Log.i(TAG, "onReceive: intent action" + intent.getAction());
+            if (intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+                //當掃描結果後，進行重新整理列表
+            } else if (intent.getAction().equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
+                //wifi連線網路狀態變化
+                NetworkInfo.DetailedState state = ((NetworkInfo) intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO)).getDetailedState();
+                getLocalIpAddress(context);
+                if(onWifiStatusListener!=null)
+                    onWifiStatusListener.wifiStatue(state);
+            } else if (intent.getAction().equals(WifiManager.WIFI_STATE_CHANGED_ACTION)) {
+                //wifi狀態變化
+                int wifiState = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_DISABLED);
+            } else if (intent.getAction().equals(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION)) {
+                SupplicantState state = intent.getParcelableExtra(WifiManager.EXTRA_NEW_STATE);
+                int error = intent.getIntExtra(EXTRA_SUPPLICANT_ERROR, 0);
+
+            }
+        }
+    };
+
+    public void superOnClick(){
+        Log.e(TAG,"NetWork onClick!");
+        Intent intent = new Intent(context, WifiMenu.class);
+        context.startActivity(intent);
+        Log.e(TAG,"ACTION_SETTINGS");
+    }
+
+    public String getLocalIpAddress(Context context) {
+
+        String ipp =  "no connect wifi!";
+        String ip =ipp;
+        WifiManager wifiMan = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInf = wifiMan.getConnectionInfo();
+        int ipAddress = wifiInf.getIpAddress();
+        ip= String.format("%d.%d.%d.%d", (ipAddress & 0xff),(ipAddress >> 8 & 0xff),(ipAddress >> 16 & 0xff),(ipAddress >> 24 & 0xff));
+
+        if(SSIDTxt!=null)
+            SSIDTxt.setText(wifiInf.getSSID());
+        if(IPTxt!=null)
+            IPTxt.setText(ip);
+        if(ip.equals(ipp)){
+            netWorkIcon.setImageDrawable(context.getResources().getDrawable(R.drawable.icon_gr));
+        }else{
+            netWorkIcon.setImageDrawable(context.getResources().getDrawable(R.drawable.icon_ge));
+        }
+
+        Log.e(TAG, "20190610 ***** IP="+ ip);
+        String theSpeech="已經連結到"+wifiInf.getSSID()+",Ip="+ip.replace(".","點");
+        Log.e(TAG, "20190610***** theSpeechIP="+ theSpeech);
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("IP", ip);
+            jsonObject.put("SSID", wifiInf.getSSID());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        }
+
+        return theSpeech;
+    }
+
+    private static String getAppVersionName(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            // should never happen
+//            throw new RuntimeException(TAG+" Could not get package name: " + e);
+            return "Could not get VersionName";
+        }
+    }
+
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // should never happen
+//            throw new RuntimeException("Could not get package name: " + e);
+            return -1;
+        }
+    }
+
+
+}
